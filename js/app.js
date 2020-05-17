3c3932f1cb02951eec45457668d2224c36b8f2d9
unknown <guptanilesh2312@gmail.com>
2020-05-17 09:22:45 +0530

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+      document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+      document.querySelector('#navbar').style.opacity = 1;
+    }
+  });
+
+  // Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 28.952321, lng: 77.103837 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
